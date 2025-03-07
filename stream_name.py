9aa17f9910e231ed51793cb17e35e7d409b936a2
FSmits <49479631+FSmits@users.noreply.github.com>
2025-03-07 11:54:31 +0100

    update filename and change location

--- stream_name.py
+++ stream_name.py
@@ -1,7 +1,7 @@
 from pyxdf import load_xdf
 
 # Load XDF file
-xdf_file = "/Users/kcmggz/Documents/CurrentStudy/sub-P009/ses-S004/eeg/sub-P009_ses-S004_task-Default_run-001_eeg.xdf"  # Replace with your file
+xdf_file = "sub-P009_ses-S004_task-Default_run-001_eeg.xdf"  # Replace with your file
 streams, _ = load_xdf(xdf_file)
 
 # Print available streams
