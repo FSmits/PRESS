de5adc0d5dddd5ad612b6d415dd2c3779fc65d9a
FSmits <49479631+FSmits@users.noreply.github.com>
2025-03-12 15:47:57 +0100

    reverse start_time and eeg_firstsample in calculation of time since eeg

--- SendStringMarkers.py
+++ SendStringMarkers.py
@@ -52,7 +52,7 @@ def main():
         # get a new sample (you can also omit the timestamp part if you're not interested in it)
         timestamp = local_clock()
         elapsed_time = timestamp - start_time
-        since_eeg_time = start_time - first_eeg_timestamp
+        since_eeg_time = first_eeg_timestamp - start_time
         latency = srate * (elapsed_time + since_eeg_time)
         markername = [random.choice(markernames)]
         print(markername, timestamp, elapsed_time, since_eeg_time, latency)
